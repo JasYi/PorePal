072240dfc2038adca3d129d37fae21c74ca3dd7a
JasYi <44511533+JasYi@users.noreply.github.com>
2025-08-08 17:46:09 -0400

    please fix?

--- frontend/porepal-frontend/next.config.mjs
+++ frontend/porepal-frontend/next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:5328/api/:path*"
-            : "/api/",
-      },
-    ];
+    return [{}];
   },
 };
 
